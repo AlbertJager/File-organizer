5b8c7f482c380c010d46a38cd93ea2ce831ae41b
AlbertJager <kalbiban22@mail.ru>
2026-08-13 21:27:02 +0300

    First commit

---
+++ organizer.py
@@ -0,0 +1,94 @@
+'''
+    Scans directory, categorizes files by extension, moves them into folders(/images, /documents, etc.), using pathlib, with a cli interface
+'''
+from pathlib import Path 
+from collections.abc import Iterator
+extensions = {
+    # Documents
+    ".txt": "texts",
+    ".pdf": "documents",
+    ".doc": "documents",
+    ".docx": "documents",
+
+    # Images
+    ".jpg": "images",
+    ".jpeg": "images",
+    ".png": "images",
+    ".gif": "images",
+    ".webp": "images",
+
+    # Videos
+    ".mp4": "videos",
+    ".mkv": "videos",
+    ".avi": "videos",
+    ".mov": "videos",
+
+    # Audio
+    ".mp3": "audio",
+    ".wav": "audio",
+    ".flac": "audio",
+
+    # Spreadsheets
+    ".csv": "spreadsheets",
+    ".xls": "spreadsheets",
+    ".xlsx": "spreadsheets",
+
+    # Archives
+    ".zip": "archives",
+    ".rar": "archives",
+    ".7z": "archives",
+    ".tar": "archives",
+    ".gz": "archives",
+
+    # Code
+    ".py": "code",
+    ".js": "code",
+    ".html": "code",
+    ".css": "code",
+}
+
+
+def scan_directory(directory: Path) -> Iterator[Path]:
+    '''Generator that scans the directory and return files, otherwise raises FileNotFoundError'''
+    try:
+        for file in directory.iterdir():
+            if file.is_file():
+                yield file
+    except FileNotFoundError as e:
+        print(f"The path '{e.filename}' is not found.")
+    except NotADirectoryError as e:
+        print(f"The path '{e.filename}' is not a directory.")
+    
+
+def categorize_file_by_extension(path: Path, file: Path) -> Path:
+    '''Returns a category corresponding to the file'''
+    if file.suffix in extensions:
+        category = path / extensions[file.suffix]
+    else:
+        category = path / "other"
+    category.mkdir(exist_ok=True)
+    return category 
+
+
+def move_into_category(file: Path, category: Path) -> None:
+    '''Moves the file into the corresponding category'''
+    file.move_into(category)
+    print(f"File '{file.name}' was successfully moved into {category}")
+
+
+def organize(path: Path) -> None:
+    '''Organizes the directory by categorizing files by extension and moving them into folder'''
+    total = 0
+    for file in scan_directory(path):
+        category = categorize_file_by_extension(path, file)
+
+        move_into_category(file, category)
+        total += 1
+
+    print(f"\nTotal files moved: {total}\n")
+
+
+if __name__ == "__main__":
+    path = Path(fr"{input("Enter the path: ")}".strip())
+    organize(path)
+    
